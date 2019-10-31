f024b720839435be27eabaa31cb608980aeae692
meil <14835841+ikonopistsev@users.noreply.github.com>
2019-10-31 16:52:49 +0200

    simplify

--- evheap/main.cpp
+++ evheap/main.cpp
@@ -33,29 +33,6 @@ void call(evutil_socket_t, short, void *)
     cout() << method_str << std::endl;
 }
 
-template<class T>
-struct fun
-{
-    static inline void call(evutil_socket_t, short, void *arg_fn) noexcept
-    {
-        assert(arg_fn);
-        auto fn = static_cast<T*>(arg_fn);
-        try
-        {
-            (*fn)();
-        } catch (...)
-        {   }
-        delete fn;
-    }
-};
-
-template<class T, class F>
-void run_once(btpro::queue& q, T timeout, F fn)
-{
-    auto arg_fn = new std::function<void()>(fn);
-    q.once(timeout, fun<std::function<void()>>::call, arg_fn);
-}
-
 int main(int, char**)
 {
     btpro::startup();
@@ -76,21 +53,16 @@ int main(int, char**)
     evh.add(std::chrono::milliseconds(300));
     evs.add(std::chrono::milliseconds(500));
 
-    run_once(q, std::chrono::milliseconds(400), []{
-        MKREFSTR(fun_str, "+fun!");
-        cout() << fun_str << std::endl;
-    });
-
     // one shot
-    auto rshot = [&](...){
-        MKREFSTR(rshot_str, "rshot");
-        cout() << rshot_str << std::endl;
+    auto lambda = [&](...){
+        MKREFSTR(lambda_str, "lambda");
+        cout() << lambda_str << std::endl;
     };
-    q.once(std::chrono::milliseconds(100), std::ref(rshot));
+    q.once(std::chrono::milliseconds(100), std::ref(lambda));
 
     q.once(std::chrono::milliseconds(110), [&](...) {
-        MKREFSTR(lambdashot_str, "lambda");
-        cout() << lambdashot_str << std::endl;
+        MKREFSTR(fn_str, "fn");
+        cout() << fn_str << std::endl;
     });
 
     MKREFSTR(run_str, "run");
